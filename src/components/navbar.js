dec9105a851c69001b2fd58f2282a952bc4e844c
peaceOffiong <peaceyben@gamil.com>
2022-09-06 22:32:30 -0700

    initial commit

---
+++ src/components/navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { FaPhoneSquare } from "react-icons/fa";
+import { Router } from 'react-router-dom';
+
+const navbar = () => {
+  return (
+    <nav>
+      <FaPhoneSquare />
+    </nav>
+  );
+}
+
+export default navbar
